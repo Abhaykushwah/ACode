286ec54981a125dbe5b65b7790e81841ef21cde3
Abhay kushwah <67948225+Abhaykushwah@users.noreply.github.com>
2022-01-20 20:49:12 +0530

    bitten_tech's folder added

---
+++ Python3/bitten_tech/lower.py
@@ -0,0 +1,9 @@
+a = input("Enter you name: ")
+b = a.lower()
+print ("lower case of your name: ", b)
+
+'''
+len(), upper(), lower(), swapcase(),
+replace(), split(), strip(), listrip(),
+rstrip(), isalpha(), isalnum(), isdigit()
+'''
