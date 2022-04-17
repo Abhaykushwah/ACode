f7aead8b468bdcde253d435a5540f08aee792ada
Abhay Kushwah <67948225+Abhaykushwah@users.noreply.github.com>
2022-04-17 23:10:22 +0530

    Added dayshname file into clang

---
+++ clang/Days-name.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main() {
+    int daynum;
+    printf("Enter the day number\n");
+    scanf("%d", &daynum);
+    switch(daynum) {
+        case 1:
+        printf("Monday");
+        break;
+        case 2:
+        printf("Tuesday");
+        break;
+        case 3:
+        printf("Wednesday");
+        break;
+        case 4:
+        printf("Thursday");
+        break;
+        case 5:
+        printf("Friday");
+        break;
+        case 6:
+        printf("Saturday");
+        break;
+        case 7:
+        printf("Sunday");
+        break;
+    }
+}
